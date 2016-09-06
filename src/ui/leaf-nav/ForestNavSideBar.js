a4deb6536e7689ade6b981b87643621a3207ae59
GreenVars <samrosen90@gmail.com>
2016-09-06 13:30:42 -0400

    Create side bar for forest navigation

---
+++ src/ui/leaf-nav/ForestNavSideBar.js
@@ -0,0 +1,32 @@
+import React, { Component, PropTypes } from 'react';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+
+const drawerStyle = {
+  overflow: 'visible',
+  width: '20rem',
+  backgroundColor: 'white',
+}
+
+export default class ForestNavSideBar extends Component {
+  static propTypes = {
+
+  }
+
+  render () {
+    return (
+      <Drawer
+        containerClassName='nav-side-bar'
+        containerStyle={ drawerStyle }
+        openSecondary
+        open
+      >
+
+      <AppBar
+        title=""
+      />
+
+      </Drawer>
+    );
+  }
+}
