066bf5a18611b1d2cf007568abdf9cf1fb145d9d
GreenVars <samrosen90@gmail.com>
2016-09-11 19:30:45 -0400

    Fix elements appearing side by side in VoteIndicator

--- src/ui/navigation/VoteIndicator.js
+++ src/ui/navigation/VoteIndicator.js
@@ -7,7 +7,9 @@ export default function VoteIndicator(props) {
       <ActionIcon className={ props.status === 1 ? "icon-up" : "icon-up-outline" }
         onClick={ props.onUp }
       />
+      <br />
       <span> { props.count } </span>
+      <br />
       <ActionIcon className={ props.status === -1 ? "icon-down" : "icon-down-outline" }
         onClick={ props.onDown }
       />
