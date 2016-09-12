98984ffd7bec74048501dbcec58e033ff987e5c4
GreenVars <samrosen90@gmail.com>
2016-09-11 21:49:06 -0400

    Make layout of leaf boxes more appealing

--- src/ui/navigation/LeafBox.js
+++ src/ui/navigation/LeafBox.js
@@ -5,18 +5,23 @@ import LeafBoxSide from './LeafBoxSide';
 export default function LeafBox(props) {
   return (
     <div className="leaf-box">
-      <LeafBoxSide onUp={ props.onUp } onDown={ props.onDown }
-        status={ props.status } count={ props.count }
-        onStar={ props.onStar }
-      />
-      <h1>
-        LEAFNAME
-        <ActionIcon className="icon-globe" onClick={ () =>console.log(5) }
-          tooltip="hi"
+      <div>
+        <LeafBoxSide onUp={ props.onUp } onDown={ props.onDown }
+          status={ props.status } count={ props.count }
+          onStar={ props.onStar }
         />
-      </h1>
-      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-      <h3> Comments </h3>
+        <h1>
+          LEAFNAME
+          <ActionIcon className="icon-globe" onClick={ () =>console.log(5) }
+            tooltip="hi"
+          />
+        </h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+      </div>
+
+      <div>
+        <h3> Comments </h3>
+      </div>
     </div>
   );
 }
